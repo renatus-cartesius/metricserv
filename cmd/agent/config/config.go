b01a5cb9f2f34c2357d17967447aaf9030b5c22d
Плясунов Михаил Игоревич <mipliasunov@domclick.ru>
2025-03-02 21:52:15 +0300

    chore: added agent config from file

--- cmd/agent/config/config.go
+++ cmd/agent/config/config.go
@@ -2,10 +2,13 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"github.com/renatus-cartesius/metricserv/pkg/logger"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Config struct {
@@ -20,7 +23,57 @@ type Config struct {
 
 func LoadConfig() (*Config, error) {
 
+	var err error
+	var ok bool
 	config := &Config{}
+	defaults := &Config{
+		SrvAddress:     "localhost:8080",
+		ReportInterval: 10,
+		PollInterval:   2,
+		AgentLogLevel:  "INFO",
+		HashKey:        "",
+		RateLimit:      2,
+		PublicKey:      "./public.pem",
+	}
+
+	configPath := "./agent.json"
+	log.Println("getting path to config from CONFIG env")
+
+	if configPath, ok = os.LookupEnv("CONFIG"); !ok {
+
+		log.Println("env CONFIG is not set, checking if -config flag passed")
+		for i, arg := range os.Args[:1] {
+			if !strings.HasPrefix(arg, "-") {
+				continue
+			}
+
+			if strings.Contains(arg, "=") {
+				if strings.Split(arg, "=")[0] == "-config" {
+					log.Println("config path found in flag")
+					configPath = strings.Split(arg, "=")[1]
+					break
+				}
+			} else {
+				if arg == "-config" {
+					log.Println("config path found in flag")
+					configPath = os.Args[i+1]
+					break
+				}
+			}
+		}
+
+	} else {
+		log.Println("config path gotten from env CONFIG")
+	}
+
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		log.Println("cannot open config file: ", err)
+	}
+
+	if err := json.NewDecoder(configFile).Decode(&defaults); err != nil {
+		logger.Log.Error("cannot unmarshall config file content")
+	}
 
 	flag.StringVar(&config.SrvAddress, "a", "localhost:8080", "address to metrics server")
 	flag.IntVar(&config.ReportInterval, "r", 10, "interval for reporting metrics to server")
@@ -29,6 +82,7 @@ func LoadConfig() (*Config, error) {
 	flag.StringVar(&config.HashKey, "k", "", "key for hashing payload")
 	flag.IntVar(&config.RateLimit, "L", 2, "amount of a parallel workers")
 	flag.StringVar(&config.PublicKey, "crypto-key", "./public.pem", "public key")
+	flag.StringVar(&configPath, "config", "./agent.json", "path to config file")
 
 	flag.Parse()
 
